b74cfa644b070b8fc7a3849992361608a5cbcf40
Adam <adamgjohnson92@gmail.com>
2023-07-27 15:50:47 -0700

    Hooked up comment routes to the models

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
 //middleware to use the imported routes and gives them a url path through /api
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes)
+
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment')
 
 //Defines relationships between Users and Posts
 User.hasMany(Post, {
